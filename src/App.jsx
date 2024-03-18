7c22eb6d0351fca6de6e09ed4cc3187a1993e70a
ataurianen <andrew.taurianen@gmail.com>
2024-03-17 20:30:30 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+const App = () => {
+  return (
+    <div className="container">
+       
+    <div className="header">
+      <div>
+        Station 1
+      </div>
+    </div>
+    <div className="body">
+
+    </div>
+    <div className="footer">
+
+    </div>
+  
+    </div>
+  ) 
+}
+
+export default App
