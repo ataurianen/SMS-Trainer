06b1d6def85f881db957238a0a3df134fffcc7ab
ataurianen <andrew.taurianen@gmail.com>
2024-03-25 22:42:33 -0700

    Some work on the header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,55 @@
+
 const App = () => {
   return (
     <div className="container">
        
-    <div className="header">
-      <div>
+    <div className='header'>
+      <div className='station-text'>
         Station 1
+      </div>
+      <div className='station-text'>
+        Station 2
+      </div>
+      <div className='station-text'>
+        Station 3
+      </div>
+       <div className='station-text'>
+        Station 4
+      </div>
+      <div className='station-text'>
+        Station 5
+      </div>
+       <div className='station-text'>
+        Station 6
+      </div>
+       <div className='station-text'>
+        Station 7
+      </div>
+      <div className='missile-grid'>
+        <div></div>
+        <div>READY</div>
+        <div>R-2</div>
+        <div></div>
+        <div>READY</div>
+        <div>R-9E</div>
+      </div>
+      <div className='station-text'>
+        test
+      </div>
+      <div className='station-text'>
+        test
+      </div>
+       <div className='station-text'>
+        test
+      </div>
+       <div className='station-text'>
+        test
+      </div>
+       <div className='station-text'>
+        test
+      </div>
+       <div className='station-text'>
+        test
       </div>
     </div>
     <div className="body">
